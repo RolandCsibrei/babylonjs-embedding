dbecbbb4763ec0ce4ad0b48867689e0806a407c4
Roland Csibrei <roland.csibrei@gmail.com>
2024-03-31 18:24:55 +0200

    readme

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,10 +2,5 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(({ command, mode }) => {
     return {
-        resolve: {
-            alias: {
-                'babylonjs': mode === 'development' ? 'babylonjs/babylon.max' : 'babylonjs'
-            }
-        }
     };
 });
